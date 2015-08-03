7d06fd3702068cfb58112a4f374530b6a9ea6018
Claudio Romano <claudiocro@gmail.com>
2015-08-03 18:34:52 +0200

    chore(ember): revert to ember 1.12.0

---
+++ config/deploy.js
@@ -0,0 +1,15 @@
+module.exports = {
+   production: {
+    store: {
+      host: 'carp.redistogo.com',
+      port: 9678,
+      password: 'b7b9cb8f19b95f77fa16ebcda96b7056'
+    },
+    assets: {
+      accessKeyId: 'AKIAJ4COS23HRJNFEECA',
+      secretAccessKey: '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',
+      bucket: 'tektronix-metacatalog',
+      prefix: 'webapp'
+    }
+  }
+};
