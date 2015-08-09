28d3cf31da5d11640da763deb9d507e3087e4a9e
Claudio Romano <claudiocro@gmail.com>
2015-08-10 01:15:32 +0200

    feat(grunt): semantic-release

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,65 @@ module.exports = function (grunt) {
         changelogText: ''
       }
     }
+  });
+
+  var getCommits = function(cb) {
+
+    var exec = require('child_process').exec;
+
+    var from = null;//lastRelease.gitHead;
+    var range = (from ? from + '..' : '') + 'HEAD';
+
+    exec(
+      'git log -E --format=%H==SPLIT==%B==END== '+range,
+      function(err, stdout) {
+        if (err) {return cb(err)};
+
+        cb(null, String(stdout).split('==END==\n')
+
+        .filter(function(raw) {return !!raw.trim();})
+
+        .map(function(raw) {
+          var data = raw.split('==SPLIT==');
+          return {
+            hash: data[0],
+            message: data[1]
+          };
+        }));
+      }
+    );
+
+  };
+
+  grunt.task.registerTask('semantic-release', 'Do a semantic relase on travis', function() {
+    //var done = this.async();
+    var git = require('conventional-changelog/lib/git');
+
+    var type = null;
+
+    getCommits(function(commits) {
+      commits.map(function(commit){
+       return git.parseRawCommit(commit.hash+'\n'+commit.message);
+      })
+
+      .filter(function(commit) { return!!commit;})
+
+      .every(function(commit) {
+        if (commit.breaks.length) {
+          type = 'major';
+          return false;
+        }
+
+        if (commit.type === 'feat') {type = 'minor'};
+
+        if (!type && commit.type === 'fix') {type = 'patch'};
+
+        return true;
+      });
+
+      console.log('type', type);
+    });
+    //cb(null, type)
 
   });
 };
