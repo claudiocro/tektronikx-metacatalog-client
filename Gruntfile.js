09717a42828a2698b16aedaf2eb33586fd6b9f6c
Claudio Romano <claudiocro@gmail.com>
2015-08-10 01:43:08 +0200

    chore(semantic-release): basic semantic-release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function (grunt) {
   'use strict';
 
+  var cp = require('child_process');
+  var git = require('conventional-changelog/lib/git');
   require('load-grunt-tasks')(grunt);
 
   // Project configuration.
@@ -22,62 +24,80 @@ module.exports = function (grunt) {
     }
   });
 
+
   var getCommits = function(cb) {
 
-    var exec = require('child_process').exec;
+console.log(git);
+    git.getLatestTag(function(err, from) {
+      console.log('from', from);
+      var range = (from ? from + '..' : '') + 'HEAD';
 
-    var from = null;//lastRelease.gitHead;
-    var range = (from ? from + '..' : '') + 'HEAD';
+      cp.exec(
+        'git log -E --format=%H==SPLIT==%B==END== '+range,
+        function(err, stdout) {
+          if (err) {return cb(err);}
 
-    exec(
-      'git log -E --format=%H==SPLIT==%B==END== '+range,
-      function(err, stdout) {
-        if (err) {return cb(err)};
+          cb(null, String(stdout).split('==END==\n')
 
-        cb(null, String(stdout).split('==END==\n')
+          .filter(function(raw) {return !!raw.trim();})
 
-        .filter(function(raw) {return !!raw.trim();})
-
-        .map(function(raw) {
-          var data = raw.split('==SPLIT==');
-          return {
-            hash: data[0],
-            message: data[1]
-          };
-        }));
-      }
-    );
-
+          .map(function(raw) {
+            var data = raw.split('==SPLIT==');
+            return {
+              hash: data[0],
+              message: data[1]
+            };
+          }));
+        }
+      );
+    });
   };
 
   grunt.task.registerTask('semantic-release', 'Do a semantic relase on travis', function() {
-    //var done = this.async();
-    var git = require('conventional-changelog/lib/git');
+    var done = this.async();
 
     var type = null;
-
-    getCommits(function(commits) {
-      commits.map(function(commit){
-       return git.parseRawCommit(commit.hash+'\n'+commit.message);
-      })
-
-      .filter(function(commit) { return!!commit;})
-
-      .every(function(commit) {
+    git.getCommits({log:console.log},function(err, commits) {
+      commits.every(function(commit) {
         if (commit.breaks.length) {
           type = 'major';
           return false;
         }
 
-        if (commit.type === 'feat') {type = 'minor'};
+        if (commit.type === 'feat') {type = 'minor';}
 
-        if (!type && commit.type === 'fix') {type = 'patch'};
+        if (!type && commit.type === 'fix') {type = 'patch';}
 
         return true;
       });
 
-      console.log('type', type);
+      console.log('Release Type', type);
+      done(type);
     });
+
+    // getCommits(function(err, commits) {
+    //   commits.map(function(commit){
+    //    return git.parseRawCommit(commit.hash+'\n'+commit.message);
+    //   })
+
+    //   .filter(function(commit) { return!!commit;})
+
+    //   .every(function(commit) {
+    //     if (commit.breaks.length) {
+    //       type = 'major';
+    //       return false;
+    //     }
+
+    //     if (commit.type === 'feat') {type = 'minor'};
+
+    //     if (!type && commit.type === 'fix') {type = 'patch'};
+
+    //     return true;
+    //   });
+
+    //   done(type);
+    //   console.log('type', type);
+    // });
     //cb(null, type)
 
   });
