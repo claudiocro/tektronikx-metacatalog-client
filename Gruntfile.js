348a37373390cd7f495c181d948a1210fae3da78
Claudio Romano <claudiocro@gmail.com>
2015-08-09 19:23:22 +0200

    chore(semantic-release): introduce semantic-release

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    release: {
+      email: 'claudiocro@gmail.com',
+      name: 'Claudio Romano'
+    }
+
+  });
+};
