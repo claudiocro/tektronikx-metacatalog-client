df14cfc6cff070a9c2064973cd629a2c9b4c1f76
Claudio Romano <claudiocro@gmail.com>
2015-08-10 02:15:09 +0200

    fix(): travis build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,3 @@
-<a name"0.2.0"></a>
-## 0.2.0 (2015-08-10)
-
-
-#### Features
-
-* **grunt:** semantic-release ([28d3cf31](https://github.com/claudiocro/tektronix-metacatalog-client/commit/28d3cf31))
-
-
 <a name"0.0.2"></a>
 ### 0.0.2 (2015-08-09)
 
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
   'use strict';
 
-  var cp = require('child_process');
   var git = require('conventional-changelog/lib/git');
   require('load-grunt-tasks')(grunt);
 
@@ -24,34 +23,9 @@ module.exports = function (grunt) {
     }
   });
 
-
-  var getCommits = function(cb) {
-
-console.log(git);
-    git.getLatestTag(function(err, from) {
-      console.log('from', from);
-      var range = (from ? from + '..' : '') + 'HEAD';
-
-      cp.exec(
-        'git log -E --format=%H==SPLIT==%B==END== '+range,
-        function(err, stdout) {
-          if (err) {return cb(err);}
-
-          cb(null, String(stdout).split('==END==\n')
-
-          .filter(function(raw) {return !!raw.trim();})
-
-          .map(function(raw) {
-            var data = raw.split('==SPLIT==');
-            return {
-              hash: data[0],
-              message: data[1]
-            };
-          }));
-        }
-      );
-    });
-  };
+  grunt.task.registerTask('foo', 'foo', function() {
+    console.log('sadf');
+  });
 
   grunt.task.registerTask('semantic-release', 'Do a semantic relase on travis', function() {
     var done = this.async();
@@ -70,35 +44,8 @@ console.log(git);
 
         return true;
       });
-
-      console.log('Release Type', type);
+      grunt.task.run('release:'+type);
       done(type);
     });
-
-    // getCommits(function(err, commits) {
-    //   commits.map(function(commit){
-    //    return git.parseRawCommit(commit.hash+'\n'+commit.message);
-    //   })
-
-    //   .filter(function(commit) { return!!commit;})
-
-    //   .every(function(commit) {
-    //     if (commit.breaks.length) {
-    //       type = 'major';
-    //       return false;
-    //     }
-
-    //     if (commit.type === 'feat') {type = 'minor'};
-
-    //     if (!type && commit.type === 'fix') {type = 'patch'};
-
-    //     return true;
-    //   });
-
-    //   done(type);
-    //   console.log('type', type);
-    // });
-    //cb(null, type)
-
   });
 };
