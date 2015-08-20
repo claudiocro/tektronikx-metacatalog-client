2e4a32d187a6db8932f4ed4f5e5854dd55906288
Claudio Romano <claudiocro@gmail.com>
2015-08-20 21:03:24 +0200

    feat: add pagecount for modelpages

--- app/models/modelpage.js
+++ app/models/modelpage.js
@@ -1,7 +1,23 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+
+const {computed} = Ember;
 
 export default DS.Model.extend({
   model: DS.belongsTo('modelhit'),
   catalog: DS.belongsTo('catalog', {async: true}),
-  page: DS.attr('number')
+  page: DS.attr('number'),
+  pagecount: DS.attr('number'),
+
+
+  pagespan: computed('page', 'pagecount', function() {
+    var pagespan = '';
+    if(this.get('page')) {
+      pagespan = this.get('page');
+      if(this.get('pagecount') && this.get('pagecount')>1) {
+        pagespan += " - " + (this.get('page') + this.get('pagecount'));
+      }
+    }
+    return pagespan;
+  })
 });
--- tests/unit/models/modelpage-test.js
+++ tests/unit/models/modelpage-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('modelpage', 'Unit | Model | modelpage', {
   // Specify the other units that are required for this test.
@@ -9,4 +10,26 @@ test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+
+test('foundInYearsRange | start and last', function(assert) {
+  var model = this.subject();
+
+  assert.equal('', model.get('pagespan'));
+
+  Ember.run(function(){
+
+    model.set('page', 20);
+    assert.equal('20', model.get('pagespan'));
+
+    model.set('pagecount', 2);
+    assert.equal('20 - 22', model.get('pagespan'));
+
+    model.set('pagecount', 1);
+    assert.equal('20', model.get('pagespan'));
+
+    model.set('pagecount', 0);
+    assert.equal('20', model.get('pagespan'));
+  });
 });
